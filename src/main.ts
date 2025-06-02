6fa1d927a9d865b5f6b2d8187a2b140cdfd80984
MikolaSh <nikolashulga2000@gmail.com>
2025-06-03 02:37:49 +0300

    feat: implements PORT from .env

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 4000;
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();
